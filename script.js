3c838846247a8b60080b3340aa1ddccd43126c32
Huskar <piedfort.q@outlook.com>
2025-07-21 13:23:14 +0200

    initial commit : Setup basic portfolio structure with HTML, CSS and JS

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+            new Typed('.animate__animated', {
+                strings: ["Bienvenue sur mon Portfolio", "Découvrez mes projets et compétences"],
+                typeSpeed: 50,
+                backSpeed: 25,
+                loop: true
+            });
+        });
+        
