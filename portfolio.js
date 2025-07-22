e509435ca6849b746adc8c38bb777113507519c3
Huskar <piedfort.q@outlook.com>
2025-07-22 22:41:34 +0200

    feat(portfolio-projects): Dynamically display HTML/CSS project status

--- portfolio.js
+++ portfolio.js
@@ -1,10 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Change the text of the h1 element after 3 seconds //
     const h1 = document.querySelector('h1');
     h1.textContent = 'Bienvenue sur mon portfolio';
     setTimeout(() => {
-        h1.textContent = 'Je suis Quentin, développeur web';
-        
+        h1.textContent = 'Je suis Quentin, développeur web'; 
     }, 3000);
+    
+    const projetHtmlCssReussi = true
+    let messageDiv = document.querySelector('#message_projet_html_css');
+    if (projetHtmlCssReussi) {
+        messageDiv.innerHTML = '<p>Félicitations ! Mon premier projet HTML/CSS est un succès !</p>';
+    } else {
+        messageDiv.innerHTML = '<p>Le projet HTML/CSS est en cours de développement...</p>';    
+    }
 });
 
         
